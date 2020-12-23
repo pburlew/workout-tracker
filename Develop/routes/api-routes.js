80c4cc8da976e3ea9f229f3f561f9da5a4506581
Paige Burlew <paigeburlew@gmail.com>
2020-12-22 19:47:42 -0600

    api routes

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -15,13 +15,40 @@ router.get("/api/workouts", (req, res) => {
 
 router.post("/api/workouts", ({ body }, res) => {
     db.Workout.create(body)
-    .then(dbWorkout => {
+    .then(dbWorkot => {
         res.json(dbWorkout);
     })
     .catch(err => {
         res.json(err);
     });
 });
+
+router.get("/api/workouts/range", ({ body }, res) => {
+    db.Workout.find()
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+  
+    })
+
+});
+
+router.get("/api/workouts/:id", ( { body} , res) => {
+    
+        const { id } = req.params;
+        db.Workout.findOne({
+            _id: id,
+        }).then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json
+        })
+   
+});
+
 
 
 
