be8d47a463317566193975678764632b603dda4c
Paige Burlew <paigeburlew@gmail.com>
2020-12-22 19:27:33 -0600

    post request

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -13,3 +13,16 @@ router.get("/api/workouts", (req, res) => {
     })
 });
 
+router.post("/api/workouts", ({ body }, res) => {
+    db.Workout.create(body)
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.json(err);
+    });
+});
+
+
+
+module.exports = router;
